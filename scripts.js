041673aa4a799bc3d8222c0aad2e1a1e3d1b22b7
Lexi Grant <lexidgrant@gmail.com>
2022-07-28 13:51:16 -0500

    got carousel to work

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ $(() => {
    
 
 
-
+// navigation bar
 buttonContent = {
     "#home": ".homeContent", "#about": ".aboutContent", "#education": ".educationContent", "#experience": ".experienceContent",
     "#projects": ".projectsContent", "#contact": ".contactContent"
@@ -18,9 +18,47 @@ for(let i = 0; i < Object.keys(buttonContent).length; i++) {
          $(buttonContent[key]).show()
     }) 
 }
-
 $(".content").hide()
-$(buttonContent["#home"]).show()
+$(buttonContent["#projects"]).show()
+
+// Lola carousel --- like example given in class
+
+let currentImage = 0
+let numOfImages = $(".carouselImages").children().length - 1
+
+
+$(".forward").on("click", () => {
+
+    $(".carouselImages").children().eq(currentImage).css("display", "none")
+
+if(currentImage < numOfImages) {
+    currentImage++
+} else {
+    currentImage = 0
+}
+$(".carouselImages").children().eq(currentImage).css("display", "block")
+
+})
+
+$(".previous").on("click", () => {
+
+$(".carouselImages").children().eq(currentImage).css("display", "none")
+
+if(currentImage > 0) {
+    currentImage--
+} else {
+    currentImage = numOfImages
+}
+
+$(".carouselImages").children().eq(currentImage).css("display", "block")
+
+
+})
+
+
+
+
+
 
 })
 
