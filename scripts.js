778ddc4ab1b862e3e355e5193a6c106c866caff0
Lexi Grant <lexidgrant@gmail.com>
2022-07-27 21:42:27 -0500

    navigation bar works

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
 
 
 $(() => {
+   
 
-const homeButton () => {
-    $("button")
+
+
+buttonContent = {
+    "#home": ".homeContent", "#about": ".aboutContent", "#education": ".educationContent", "#experience": ".experienceContent",
+    "#projects": ".projectsContent", "#contact": ".contactContent"
 }
 
 
+for(let i = 0; i < Object.keys(buttonContent).length; i++) {
+    const key = Object.keys(buttonContent)[i]
+    $(key).on("click",() => {
+        $(".content").hide()
+         $(buttonContent[key]).show()
+    }) 
+}
+
+$(".content").hide()
+$(buttonContent["#home"]).show()
+
 })
+
